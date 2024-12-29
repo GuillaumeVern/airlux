50f59fb98136caff579f5e81c0f96ea794d49296
GuillaumeVern <guillaume.vern@outlook.com>
2024-12-29 19:07:16 +0100

    ytfuybjnlk

--- NAT/raspberry-pi/script-launcher/start.bash
+++ NAT/raspberry-pi/script-launcher/start.bash
@@ -24,7 +24,7 @@ ls /etc/ssh
 RSA="$(cat /etc/ssh/raspberry_rsa.pub)"
 echo "Clé publique: $RSA"
 
-echo "n" | ssh-keygen -t rsa -b 4096 -f ~/.ssh -N ""
+echo "n" | ssh-keygen -t rsa -b 4096 -f /home/g3/.ssh -N ""
 
 
 curl -X POST -H "Content-Type: application/json" -d "{\"Adresse_MAC\":\"$MAC\", \"Pub_Key\": \"$RSA\"}" http://212.83.130.156:8000/raspberry
@@ -40,7 +40,7 @@ PORT="$(curl -X GET http://212.83.130.156:8000/raspberry/$MAC/port | jq -r '.por
 # PORT="$(curl -X GET http://airnet-api:8000/raspberry/$MAC/port | jq -r '.port')"
 echo "Port: $PORT"
 
-ssh-keyscan -t rsa 212.83.130.156 >> ~/.ssh/authorized_keys
+ssh-keyscan -t rsa 212.83.130.156 >> /home/g3/.ssh/authorized_keys
 
 
 ssh -Nfvvvv -R "$PORT:localhost:22" g3@212.83.130.156 -i /etc/ssh/raspberry_rsa -o StrictHostKeyChecking=no
