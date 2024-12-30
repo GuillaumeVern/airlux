88ed94ec127a46b4c377dcee98ff2be3f3b44624
GuillaumeVern <guillaume.vern@outlook.com>
2024-12-30 09:55:59 +0100

    cut cut

--- NAT/raspberry-pi/script-launcher/start.bash
+++ NAT/raspberry-pi/script-launcher/start.bash
@@ -22,7 +22,7 @@ sudo systemctl start ssh
 echo "n" | sudo -u user ssh-keygen -t rsa -b 4096 -f /etc/ssh/id_rsa -N ""
 
 ls /etc/ssh
-RSA="$(ssh-keyscan -t rsa localhost | cut -d " " -f 2)"
+RSA="$(ssh-keyscan -t rsa localhost | cut -d " " -f 1)"
 echo "Clé publique: $RSA"
 
 
