237c46a8d61e7cb60b79797086be3283214e099f
GuillaumeVern <guillaume.vern@outlook.com>
2024-12-30 18:39:13 +0100

    key duplication management

--- public-server/airnet-private-server/main.py
+++ public-server/airnet-private-server/main.py
@@ -23,9 +23,11 @@ def add_key(key: Key):
     try:
         create_file_if_not_exists()
         with open("/config/.ssh/authorized_keys", "a") as f:
-            f.write(key.key + "\n")
+            if key.key not in f.read():
+                f.write(key.key + "\n")
         with open("/config/.ssh/known_hosts", "a") as f:
-            f.write(key.key + "\n")
+            if key.key not in f.read():
+                f.write(key.key + "\n")
         return JSONResponse(content={"message": "Key added successfully"}, status_code=201)
     except Exception as e:
         return JSONResponse(content={"message": e}, status_code=500)
