2366256554bd2bc666427324144ea5362943233e
GuillaumeVern <guillaume.vern@outlook.com>
2025-01-20 11:27:56 +0100

    ping

--- NAT/raspberry-pi/manager/main.py
+++ NAT/raspberry-pi/manager/main.py
@@ -82,7 +82,7 @@ def subscribe_to_all(client: mqtt_client):
             },
             "time": decoded_payload["timestamp"]
         }
-        if inf_client.health().status == 'passing':
+        if inf_client.ping():
             for key in red_client.scan_iter():
                 result = red_client.json().get(key)
                 red_client.delete(key)
